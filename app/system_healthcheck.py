7dfea2d1e14ea2cdb94badc2317b9b7eeb7cfa14
Kosta Tenedios <kosta.tenedios@gmail.com>
2018-04-12 00:21:18 +1000

    Added reporting of application
    Reporting of application has been added. Reliant on an IO object capable of reading a JSON file. This dependency has been absctracted so that unit testing is made easier. This JSON file is expected to be generated by the build process.

--- app/system_healthcheck.py
+++ app/system_healthcheck.py
@@ -1,7 +1,9 @@
+import json
 from app.inversion_of_control import Component, HasAttributes, HasMethods, RequiredFeature
 
 class SystemHealthCheck(Component):
     http_request = RequiredFeature('HttpRequest', HasAttributes('status_code') and HasMethods('get'))
+    file_reader = RequiredFeature('FileReader', HasMethods('read'))
 
     def __init__(self, url_to_check):
         self.url_to_check = url_to_check
@@ -12,3 +14,6 @@ class SystemHealthCheck(Component):
             return True, 'URL %s OK' % self.url_to_check
         else:
             return False, 'URL %s did not return HTTP OK' % self.url_to_check
+
+    def get_application_data(self):
+        return json.load(self.file_reader)
--- tests/test_system_healthcheck.py
+++ tests/test_system_healthcheck.py
@@ -17,12 +17,23 @@ class MockHttpRequests():
         # Caller is expecting an object to be returned that contains the attribute status_code
         return self
 
+class MockFileOpen():
+    def __init__(self, data_to_return):
+        self.data_to_return = data_to_return
+
+    def read(self):
+        return self.data_to_return
+
 class TestSystemHealthcheck(unittest.TestCase):
+    mock_file_data = '{"app": {"testcase": "Read JSON file", "author": "Unit Tester", "date": "Wed 10 Jan 2000", "commitsha": "abcd1234"}}'
 
     def setUp(self):
         if not 'HttpRequest' in features.providers:
             features.Provide('HttpRequest', MockHttpRequests)
-    
+
+        if not 'FileReader' in features.providers:
+            features.Provide('FileReader', MockFileOpen, data_to_return=self.mock_file_data)
+
     def test_check_url_returns_200_for_valid_page(self):
         url_to_check = 'http://unittesting.com'
         expected_message = 'URL %s OK' % url_to_check
@@ -30,7 +41,7 @@ class TestSystemHealthcheck(unittest.TestCase):
         result = health_check.check_url()
         self.assertEqual(True, result[0], 'Expecting True to be returned for %s' % url_to_check)
         self.assertEqual(expected_message, result[1], 'Incorrect message returned')
-    
+
     def test_check_url_does_not_return_200_for_invalid_page(self):
         url_to_check = 'http://invalidpage.com'
         expected_message = 'URL %s did not return HTTP OK' % url_to_check
@@ -38,6 +49,52 @@ class TestSystemHealthcheck(unittest.TestCase):
         result = health_check.check_url()
         self.assertEqual(False, result[0], 'Expecting False to be returned for %s' % url_to_check)
         self.assertEqual(expected_message, result[1], 'Incorrect message returned')
-    
+
+    def test_get_application_data_contains_app_key(self):
+        key = 'app'
+        health_check = SystemHealthCheck('http://someurl.com')
+        json_data = health_check.get_application_data()
+        self.assertEqual(True, key in json_data, 'Expecting %s to have been in json_data' % key)
+
+    def test_get_application_data_does_not_contain_yada_key(self):
+        key = 'yada'
+        health_check = SystemHealthCheck('http://someurl.com')
+        json_data = health_check.get_application_data()
+        self.assertEqual(False, key in json_data, 'Expecting %s to not have been in json_data' % key)
+
+    def test_get_application_data_contains_testcase_key_within_app(self):
+        key = 'testcase'
+        health_check = SystemHealthCheck('http://someurl.com')
+        json_data = health_check.get_application_data()
+        self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
+        self.assertEqual('Read JSON file', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
+
+    def test_get_application_data_contains_author_key_within_app(self):
+        key = 'author'
+        health_check = SystemHealthCheck('http://someurl.com')
+        json_data = health_check.get_application_data()
+        self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
+        self.assertEqual('Unit Tester', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
+
+    def test_get_application_data_contains_date_key_within_app(self):
+        key = 'date'
+        health_check = SystemHealthCheck('http://someurl.com')
+        json_data = health_check.get_application_data()
+        self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
+        self.assertEqual('Wed 10 Jan 2000', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
+
+    def test_get_application_data_contains_commitsha_key_within_app(self):
+        key = 'commitsha'
+        health_check = SystemHealthCheck('http://someurl.com')
+        json_data = health_check.get_application_data()
+        self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
+        self.assertEqual('abcd1234', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
+
+    def test_get_application_data_does_not_contain_bla_key_within_app(self):
+        key = 'bla'
+        health_check = SystemHealthCheck('http://someurl.com')
+        json_data = health_check.get_application_data()
+        self.assertEqual(False, key in json_data['app'], 'Expecting %s to not have been in json_data[\'app\']' % key)
+
 if __name__ == '__main__':
     unittest.main()
