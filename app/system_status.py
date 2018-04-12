e62af332b12e42cefaa4769f9644ab326c18e3f8
Kosta Tenedios <kosta.tenedios@gmail.com>
2018-04-13 01:09:13 +1000

    More unit tests for SystemStatus
    More unit tests were added for SystemStatus, to verify that the correct health check method is added to the health checker, and to verify that the correct application dump method is added to the environment dump object.
    
    Code improvements made for readability, including renaming classes.

---
+++ app/system_status.py
@@ -0,0 +1,24 @@
+import json
+from app.inversion_of_control import Component, HasAttributes, HasMethods, RequiredFeature
+
+class SystemStatus(Component):
+    _http_request = RequiredFeature('HttpRequest', HasAttributes('status_code') and HasMethods('get'))
+    _file_reader = RequiredFeature('FileReader', HasMethods('read'))
+    _health_check = RequiredFeature('HealthCheck', HasMethods('add_check'))
+    _environment_dump = RequiredFeature('EnvironmentDump', HasMethods('add_section'))
+    _application_section_name = RequiredFeature('ApplicationSectionName')
+
+    def __init__(self, url_to_check):
+        self._url_to_check = url_to_check
+        self._health_check.add_check(self.check_url)
+        self._environment_dump.add_section(self._application_section_name, self.get_application_data)
+
+    def check_url(self):
+        response = self._http_request.get(self._url_to_check)
+        if response.status_code == 200:
+            return True, 'URL %s OK' % self._url_to_check
+        else:
+            return False, 'URL %s did not return HTTP OK' % self._url_to_check
+
+    def get_application_data(self):
+        return json.load(self._file_reader)
--- tests/test_system_status.py
+++ tests/test_system_status.py
@@ -1,13 +1,11 @@
 import unittest
 from app.inversion_of_control import features
-from app.system_healthcheck import SystemHealthCheck
+from app.system_status import SystemStatus
 
 class MockHttpRequests():
-    status_code = 0
-
     def __init__(self):
-        pass
-    
+        self.status_code = 0
+
     def get(self, url_to_check):
         if url_to_check == 'http://unittesting.com':
             self.status_code = 200
@@ -19,82 +17,128 @@ class MockHttpRequests():
 
 class MockFileOpen():
     def __init__(self, data_to_return):
-        self.data_to_return = data_to_return
+        self._data_to_return = data_to_return
 
     def read(self):
-        return self.data_to_return
+        return self._data_to_return
+
+class MockHealthCheck():
+    def __init__(self):
+        self._health_check_method = None
+
+    def add_check(self, health_check_method):
+        self._health_check_method = health_check_method
+
+    def get_health_check_method(self):
+        return self._health_check_method
+
+class MockEnvironmentDump():
+    def __init__(self):
+        self._section_name = None
+        self._section_method = None
+
+    def add_section(self, section_name, section_method):
+        self._section_name = section_name
+        self._section_method = section_method
+
+    def get_section_name(self):
+        return self._section_name
+
+    def get_section_method(self):
+        return self._section_method
 
 class TestSystemHealthcheck(unittest.TestCase):
-    mock_file_data = '{"app": {"testcase": "Read JSON file", "author": "Unit Tester", "date": "Wed 10 Jan 2000", "commitsha": "abcd1234"}}'
+    _mock_file_data = '{"app": {"testcase": "Read JSON file", "author": "Unit Tester", "date": "Wed 10 Jan 2000", "commitsha": "abcd1234"}}'
+    _health_check = MockHealthCheck()
+    _environment_dump = MockEnvironmentDump()
+    _application_section_name = 'Application'
 
     def setUp(self):
         if not 'HttpRequest' in features.providers:
             features.Provide('HttpRequest', MockHttpRequests)
 
         if not 'FileReader' in features.providers:
-            features.Provide('FileReader', MockFileOpen, data_to_return=self.mock_file_data)
+            features.Provide('FileReader', MockFileOpen, data_to_return=self._mock_file_data)
+
+        if not 'HealthCheck' in features.providers:
+            features.Provide('HealthCheck', self._health_check)
+
+        if not 'EnvironmentDump' in features.providers:
+            features.Provide('EnvironmentDump', self._environment_dump)
+
+        if not 'ApplicationSectionName' in features.providers:
+            features.Provide('ApplicationSectionName', self._application_section_name)
 
     def test_check_url_returns_200_for_valid_page(self):
         url_to_check = 'http://unittesting.com'
         expected_message = 'URL %s OK' % url_to_check
-        health_check = SystemHealthCheck(url_to_check)
-        result = health_check.check_url()
+        system_status = SystemStatus(url_to_check)
+        result = system_status.check_url()
         self.assertEqual(True, result[0], 'Expecting True to be returned for %s' % url_to_check)
         self.assertEqual(expected_message, result[1], 'Incorrect message returned')
 
     def test_check_url_does_not_return_200_for_invalid_page(self):
         url_to_check = 'http://invalidpage.com'
         expected_message = 'URL %s did not return HTTP OK' % url_to_check
-        health_check = SystemHealthCheck(url_to_check)
-        result = health_check.check_url()
+        system_status = SystemStatus(url_to_check)
+        result = system_status.check_url()
         self.assertEqual(False, result[0], 'Expecting False to be returned for %s' % url_to_check)
         self.assertEqual(expected_message, result[1], 'Incorrect message returned')
 
     def test_get_application_data_contains_app_key(self):
         key = 'app'
-        health_check = SystemHealthCheck('http://someurl.com')
-        json_data = health_check.get_application_data()
+        system_status = SystemStatus('http://someurl.com')
+        json_data = system_status.get_application_data()
         self.assertEqual(True, key in json_data, 'Expecting %s to have been in json_data' % key)
 
     def test_get_application_data_does_not_contain_yada_key(self):
         key = 'yada'
-        health_check = SystemHealthCheck('http://someurl.com')
-        json_data = health_check.get_application_data()
+        system_status = SystemStatus('http://someurl.com')
+        json_data = system_status.get_application_data()
         self.assertEqual(False, key in json_data, 'Expecting %s to not have been in json_data' % key)
 
     def test_get_application_data_contains_testcase_key_within_app(self):
         key = 'testcase'
-        health_check = SystemHealthCheck('http://someurl.com')
-        json_data = health_check.get_application_data()
+        system_status = SystemStatus('http://someurl.com')
+        json_data = system_status.get_application_data()
         self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
         self.assertEqual('Read JSON file', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
 
     def test_get_application_data_contains_author_key_within_app(self):
         key = 'author'
-        health_check = SystemHealthCheck('http://someurl.com')
-        json_data = health_check.get_application_data()
+        system_status = SystemStatus('http://someurl.com')
+        json_data = system_status.get_application_data()
         self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
         self.assertEqual('Unit Tester', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
 
     def test_get_application_data_contains_date_key_within_app(self):
         key = 'date'
-        health_check = SystemHealthCheck('http://someurl.com')
-        json_data = health_check.get_application_data()
+        system_status = SystemStatus('http://someurl.com')
+        json_data = system_status.get_application_data()
         self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
         self.assertEqual('Wed 10 Jan 2000', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
 
     def test_get_application_data_contains_commitsha_key_within_app(self):
         key = 'commitsha'
-        health_check = SystemHealthCheck('http://someurl.com')
-        json_data = health_check.get_application_data()
+        system_status = SystemStatus('http://someurl.com')
+        json_data = system_status.get_application_data()
         self.assertEqual(True, key in json_data['app'], 'Expecting %s to have been in json_data[\'app\']' % key)
         self.assertEqual('abcd1234', json_data['app'][key], 'json_data[\'app\'][\'%s\'] not returning expected string' % key)
 
     def test_get_application_data_does_not_contain_bla_key_within_app(self):
         key = 'bla'
-        health_check = SystemHealthCheck('http://someurl.com')
-        json_data = health_check.get_application_data()
+        system_status = SystemStatus('http://someurl.com')
+        json_data = system_status.get_application_data()
         self.assertEqual(False, key in json_data['app'], 'Expecting %s to not have been in json_data[\'app\']' % key)
+
+    def test_health_check_method_added(self):
+        system_status = SystemStatus('http://someurl.com')
+        self.assertEqual(system_status.check_url, self._health_check.get_health_check_method(), 'Incorrect health check method')
+
+    def test_environment_dump_method_added(self):
+        system_status = SystemStatus('http://someurl.com')
+        self.assertEqual(system_status.get_application_data, self._environment_dump.get_section_method(), 'Incorrect environment dump method')
+        self.assertEqual(self._application_section_name, self._environment_dump.get_section_name(), 'Incorrect section name')
 
 if __name__ == '__main__':
     unittest.main()
