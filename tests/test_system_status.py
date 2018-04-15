a2b4001fea3c20fa5277adf62ace2c51f0526201
Kosta Tenedios <kosta.tenedios@gmail.com>
2018-04-16 00:11:40 +1000

    Updates SystemStatus tests
    SystemStatus tests were updated as these initially neglected that the HealthCheck and EnvironmentDump depencencies required an application (i.e. Flask) and a path for reporting on health status and providing an environment dump. Testing that the HealthCheck and EnvironmentDump objects have the application and path injected into then required other tests to be updated.

--- tests/test_system_status.py
+++ tests/test_system_status.py
@@ -1,6 +1,38 @@
 import unittest
 from app.inversion_of_control import features
 from app.system_status import SystemStatus
+from tests.mock_application import MockApplication
+
+# Global variables and static methods for validating behaviours
+_health_check_method = None
+_section_name = None
+_section_method = None
+_application_for_health_check = None
+_path_for_health_check = None
+_application_for_environment_dump = None
+_path_for_environment_dump = None
+
+def set_global_health_check_method(health_check_method):
+    global _health_check_method
+    _health_check_method = health_check_method
+
+def set_global_section_properties(section_name, section_method):
+    global _section_name
+    global _section_method
+    _section_name = section_name
+    _section_method = section_method
+
+def set_global_application_and_path_for_health_check(application, path):
+    global _application_for_health_check
+    global _path_for_health_check
+    _application_for_health_check = application
+    _path_for_health_check = path
+
+def set_global_application_and_path_for_environment_dump(application, path):
+    global _application_for_environment_dump
+    global _path_for_environment_dump
+    _application_for_environment_dump = application
+    _path_for_environment_dump = path
 
 class MockHttpRequests():
     def __init__(self):
@@ -23,43 +55,31 @@ class MockFileOpen():
         return self._data_to_return
 
 class MockHealthCheck():
-    def __init__(self):
-        self._health_check_method = None
+    def __init__(self, app, path):
+        set_global_application_and_path_for_health_check(app, path)
 
     def add_check(self, health_check_method):
-        self._health_check_method = health_check_method
-
-    def get_health_check_method(self):
-        return self._health_check_method
+        set_global_health_check_method(health_check_method)
 
 class MockEnvironmentDump():
-    def __init__(self):
-        self._section_name = None
-        self._section_method = None
+    def __init__(self, app, path):
+        set_global_application_and_path_for_environment_dump(app, path)
 
     def add_section(self, section_name, section_method):
-        self._section_name = section_name
-        self._section_method = section_method
-
-    def get_section_name(self):
-        return self._section_name
-
-    def get_section_method(self):
-        return self._section_method
+        set_global_section_properties(section_name, section_method)
 
 class TestSystemHealthcheck(unittest.TestCase):
     _mock_file_data = '{"app": {"testcase": "Read JSON file", "author": "Unit Tester", "date": "Wed 10 Jan 2000", "commitsha": "abcd1234"}}'
-    _health_check = MockHealthCheck()
-    _environment_dump = MockEnvironmentDump()
     _application_section_name = 'Application'
+    _application = MockApplication('TestSystemHealthcheck')
 
     def setUp(self):
         # Allow the dependencies to be replaced so as not to affect unit tests in other test classes
         features.allowReplace = True
         features.Provide('HttpRequest', MockHttpRequests)
         features.Provide('FileReader', MockFileOpen, data_to_return=self._mock_file_data)
-        features.Provide('HealthCheck', self._health_check)
-        features.Provide('EnvironmentDump', self._environment_dump)
+        features.Provide('HealthCheck', MockHealthCheck, app=self._application, path='/testHealthCheck')
+        features.Provide('EnvironmentDump', MockEnvironmentDump, app=self._application, path='/testEnvironmentDump')
         features.Provide('ApplicationSectionName', self._application_section_name)
 
     def test_check_url_returns_200_for_valid_page(self):
@@ -126,12 +146,19 @@ class TestSystemHealthcheck(unittest.TestCase):
 
     def test_health_check_method_added(self):
         system_status = SystemStatus('http://someurl.com')
-        self.assertEqual(system_status.check_url, self._health_check.get_health_check_method(), 'Incorrect health check method')
+        self.assertEqual(system_status.check_url, _health_check_method, 'Incorrect health check method')
 
     def test_environment_dump_method_added(self):
         system_status = SystemStatus('http://someurl.com')
-        self.assertEqual(system_status.get_application_data, self._environment_dump.get_section_method(), 'Incorrect environment dump method')
-        self.assertEqual(self._application_section_name, self._environment_dump.get_section_name(), 'Incorrect section name')
+        self.assertEqual(system_status.get_application_data, _section_method, 'Incorrect environment dump method')
+        self.assertEqual(self._application_section_name, _section_name, 'Incorrect section name')
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_health_check_application_and_path_are_injected(self):
+        SystemStatus('http://someurl.com')
+        self.assertEqual(self._application, _application_for_health_check, 'The wrong application was injected')
+        self.assertEqual('/testHealthCheck', _path_for_health_check, 'The wrong path was injected')
+
+    def test_environment_dump_application_and_path_are_injected(self):
+        SystemStatus('http://someurl.com')
+        self.assertEqual(self._application, _application_for_environment_dump, 'The wrong application was injected')
+        self.assertEqual('/testEnvironmentDump', _path_for_environment_dump, 'The wrong path was injected')
