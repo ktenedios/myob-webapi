7479c3ce50fa960e8fed8d7153ddef3b8a85f7e3
Kosta Tenedios <kosta.tenedios@gmail.com>
2018-04-18 14:43:47 +1000

    Incorrect URL to Heroku repository
    The file `.circleci/setup-heroku.sh` was meant to have had the URL to the Heroku git repository. Instead it had the URL to the GitHub repository, which caused the deployment to fail, as the SSH key did not have write access to the GitHub repository.

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -2,7 +2,7 @@
 
 # Contents copied from https://circleci.com/docs/2.0/project-walkthrough/#deploying-to-heroku,
 # with the only change being the URL to the myob-webapi GitHub repository
-git remote add heroku https://github.com/ktenedios/myob-webapi.git
+git remote add heroku https://git.heroku.com/ktenedios-football-results-api.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
